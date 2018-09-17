0585c4cba5bdc1011e3971cec9b50765e5cd64de
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-09-18 00:25:47 +0200

    Add POST /users/login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/key.json");
 //Load user model
 const User = require("../../models/User");
 // @route /GET api/users/test
@@ -36,5 +38,32 @@ router.post("/register", (req, res) => {
     })
     .then(user => res.status(200).json({ user }))
     .catch(err => res.status(400).json(err.message));
+});
+// @route /POST api/users/login
+// @desc Login User / Returning JWT Token
+// @acces Public
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ email: "User not found" });
+      }
+      bcrypt.compare(password, user.password).then(isCorrect => {
+        if (isCorrect) {
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar
+          };
+          jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
+            res.json({ success: true, token });
+          });
+        } else {
+          res.status(401).json({ password: "Password not correct" });
+        }
+      });
+    })
+    .catch(e => res.status(500).json({ msg: "Something went wrong" }));
 });
 module.exports = router;
