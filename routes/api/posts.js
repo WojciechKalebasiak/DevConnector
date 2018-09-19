e318d201b7765637282816957e42699617dcdfa3
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-09-19 04:41:25 +0200

    Add GET routes for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,9 +6,10 @@ const Post = require("../../models/Post");
 
 //Validation
 const validatePost = require("../../validation/post");
+
 // @route /POST api/posts
 // @desc Create post
-// @acces Public
+// @acces Private
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
@@ -29,4 +30,33 @@ router.post(
       .catch(err => res.status(500).json(err));
   }
 );
+
+// @route /GET api/posts
+// @desc Get all posts
+// @acces Public
+router.get("/", (req, res) => {
+  Post.find({})
+    .sort({ date: -1 })
+    .then(posts => {
+      if(!posts) {
+        return res.status(404).json({nopostfound:"No posts found"});
+      }
+      res.json(posts);
+    })
+    .catch(err => res.status(404).json({nopostfound:"No posts found"}));
+});
+
+// @route /GET api/posts/:post_id
+// @desc Get post by id
+// @acces Public
+router.get("/:post_id", (req, res) => {
+  Post.findById(req.params.post_id)
+    .then(post => {
+      if(!post) {
+        return res.status(404).json({nopostfound:"No post found with that ID"});
+      }
+      res.json(post);
+    })
+    .catch(err => res.status(404).json({nopostfound:"No post found with that ID"}));
+});
 module.exports = router;
