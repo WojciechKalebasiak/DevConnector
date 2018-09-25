99a1401cc1ff5b290c12ee1e31da9038b14f3873
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-09-25 21:30:11 +0200

    Add loading spinner

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,26 +1,59 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentUser } from "../../actions/profileActions";
+import Spinner from "../common/Spinner";
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentUser();
   }
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+    let dashboardContent;
+    if (profile === null || loading === true) {
+      dashboardContent = <Spinner />;
+    } else {
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>DISPLAY PROFILE</h4>;
+      } else {
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcome {user.name}</p>
+            <p>You have not created profile, please add some info</p>
+            <Link to="/create-brofile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        );
+      }
+    }
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="cointainer">
+          <div className="col-md-12">
+            <h1 className="display-4">Dashboard</h1>
+            {dashboardContent}
+          </div>
+        </div>
       </div>
     );
   }
 }
 Dashboard.propTypes = {
-  getCurrentUser: PropTypes.func.isRequired
+  getCurrentUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
 const mapDispatchToProps = dispatch => ({
   getCurrentUser: () => dispatch(getCurrentUser())
 });
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Dashboard);
